fff7626da5b322f118ee54a040215d2ef09b427b
stescobedo92 <stescobedo.31@gmail.com>
2023-04-20 10:16:47 -0600

    Add basic configuration for data context

---
+++ EFReletionships/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFReletionships.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
